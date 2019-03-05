00744e282b435050c0bf8b45dd31b6f2c113d8f3
Khoa Nguyen <khoand2807@gmail.com>
2019-03-05 19:42:59 +0700

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{'name': 'Multiuser To-Do',
+ 'description': 'Extend the To-Do app to multiuser.',
+ 'author': 'Daniel Reis',
+ 'depends': ['todo_app', 'mail'],
+ 'data': [
+     'security/todo_access_rules.xml',
+     'views/todo_task.xml',
+ ],
+ }
